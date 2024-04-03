62817ae7ee92cd1e8b5325f5a8ebe2a2efc11f15
vvalleejo <108023670+vvalleejo@users.noreply.github.com>
2024-04-03 23:46:24 +0200

    changes: - creation of 6 different scenarios with different train_test ratio and window size - cration of internal methods to calculate the min, max and mean - loop to create the pickle files for each stock - adding arguments to pass the date for the filename structure and the number of the scenario to process

--- src/DataPreprocessing/NewDataPreprocessing.py
+++ src/DataPreprocessing/NewDataPreprocessing.py
@@ -24,45 +24,30 @@ warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
 class Stock:
-    def __init__(self, ticker, data, lahead, tr_tst):
+    def __init__(self, ticker, file_name, lahead, tr_tst):
         self.ticker = ticker
-        self._data = data
+        self._data = self._read_data(file_name)
         self._lahead = lahead
         self._tr_tst = tr_tst
         self.df = pd.DataFrame(self._data)
         self.serial_dict = {}
         self.mserial_dict = {}
 
-    def normalize_data(self, mXl, avmX, mYl, dfx, idx, ahead):
+    def _read_data(self, file_name):
         '''
-        Returns the normalized data and the mean, min, and max values of the data
+        Returns the data from the data path
         
-        Arguments:
-        mXl - multivariate data
-        avmX - average of the multivariate data
-        mYl - target data
-        idx - index of the data
-        ahead - ahead value
+        Parameters:
+        file_name - path to the data
         '''
-
-        avmXc = mXl - avmX[:, None, :]
-        pavX = pd.DataFrame(np.mean(mXl, axis=1), columns=dfx.columns)
-        pavX.set_index(idx[ahead+1:], drop=True, inplace=True)
-        mxmX = np.round(np.max(avmXc, axis=(0, 1)), 2)
-        mnmX = np.round(np.min(avmXc, axis=(0, 1)), 2)
-        mvdd = {"mean": pavX, "min": mnmX, "max": mxmX}
-        mXn = (avmXc - mnmX[None, None, :]) / (mxmX[None, None, :] - mnmX[None, None, :] + 0.00001)
-        mYn = ((mYl.to_numpy() - avmX[:, 0]) - mnmX[0]) / (mxmX[0] - mnmX[0] + 0.00001)
-        return mXn, mYn, mvdd
+        return pd.read_csv(file_name, sep=",", index_col=0, parse_dates=True)
 
     def process_univariate_data(self, win):
-        """
-        Process univariate data for the stock.
-
+        '''
         Parameters:
         - win (int): Window size.
         - tr_tst (float): Train-test ratio.
-        """
+        '''
         for ahead in self._lahead:
             df = self.df
 
@@ -73,13 +58,13 @@ class Stock:
             X = X.iloc[:-(ahead + 1), :]
             X.set_index(df.index[(win - 1):-(ahead + 1)], drop=True, inplace=True)
 
-            xm = X.mean(axis=1)
-            cX = X.sub(X.mean(axis=1), axis=0)
+            xm = self.calculate_mean(X, axis=1)
+            cX = X.sub(xm, axis=0)
 
-            mnx = round(min(cX.min()))
-            mxx = round(max(cX.max()))
+            mnx = round(min(self.calculate_min(cX)))
+            mxx = round(min(self.calculate_max(cX)))
             vdd = pd.DataFrame({'mean': xm, 'min': mnx, 'max': mxx})
-            vdd.set_index(X.index, drop=True, inplace=True)
+            vdd.set_index(X.index)
 
             cXn = cX.apply(lambda x: (x - mnx) / (mxx - mnx), axis=1)
             cYn = pd.Series([((i - j) - mnx) / (mxx - mnx) for i, j in zip(Y.tolist(), xm.tolist())], index=Y.index)
@@ -99,7 +84,7 @@ class Stock:
 
     def process_multivariate_data(self, mwin, m_ftrs):
         '''
-        Arguments:
+        Parameters:
         mwin - multivariate window size
         m_ftrs - multivariate number of features
         tr_tst - train-test ratio
@@ -116,13 +101,13 @@ class Stock:
         for ahead in self._lahead:
             mXl = mX[:-(ahead + 1), :, :]
             mYl = dfY.iloc[ahead + mwin:]
-            avmX = np.mean(mXl, axis=1)
+            avmX = self.calculate_mean(mXl, axis=1)
 
             avmXc = mXl - avmX[:, None, :]
-            pavX = pd.DataFrame(np.mean(mXl, axis=1), columns=dfX.columns)
+            pavX = pd.DataFrame(avmX, columns=dfX.columns)
             pavX.set_index(idx[ahead+1:], drop=True, inplace=True)
-            mxmX = np.round(np.max(avmXc, axis=(0, 1)), 2)
-            mnmX = np.round(np.min(avmXc, axis=(0, 1)), 2)
+            mxmX = np.round(self.calculate_max(avmXc, axis=(0, 1)), 2)
+            mnmX = np.round(self.calculate_min(avmXc, axis=(0, 1)), 2)
             mvdd = {"mean": pavX, "min": mnmX, "max": mxmX}
             mXn = (avmXc - mnmX[None, None, :]) / (mxmX[None, None, :] - mnmX[None, None, :] + 0.00001)
             mYn = ((mYl.to_numpy() - avmX[:, 0]) - mnmX[0]) / (mxmX[0] - mnmX[0] + 0.00001)
@@ -140,7 +125,16 @@ class Stock:
                                         "testX": mtestX, "testY": mtestY, "vdd": mvdd, "cnms": cols,
                                         "idtest": xdx[pmod:]}
 
-    def prepare_multivariate_data(self, dfX, win, n_ftrs, idx): # prepare multivariate data for modeling with window size and number of features
+    def prepare_multivariate_data(self, dfX, win, n_ftrs, idx):
+        '''
+        Returns the multivariate data for modeling
+        
+        Parameters:
+        dfX - multivariate features data
+        win - window size
+        n_ftrs - number of features
+        idx - index of the data
+        '''
         mX = []
         cols = dfX.columns
         for i in range(len(dfX.columns)):
@@ -152,22 +146,37 @@ class Stock:
         return mX, cols
 
     def compute_sentiment_scores(self):
+        '''
+        Computes the sentiment scores for the data
+        '''
         self.df['TWEET_POSTIVIE'] = self.df['TWITTER_POS_SENTIMENT_COUNT'] / self.df['TWITTER_PUBLICATION_COUNT'].replace(0, 1)
         self.df['TWEET_NEGATIVE'] = self.df['TWITTER_NEG_SENTIMENT_COUNT'] / self.df['TWITTER_PUBLICATION_COUNT'].replace(0, 1)
         self.df['NEWS_POSITIVE'] = self.df['NEWS_POS_SENTIMENT_COUNT'] / self.df['NEWS_PUBLICATION_COUNT'].replace(0, 1)
         self.df['NEWS_NEGATIVE'] = self.df['NEWS_NEG_SENTIMENT_COUNT'] / self.df['NEWS_PUBLICATION_COUNT'].replace(0, 1)
 
     def compute_volatility(self):
+        '''
+        Computes the volatility for the data with the Garman and Klass model
+        '''
         self.df['VOLATILITY'] = 1/2 * (np.log(self.df["PX_HIGH"]) - np.log(self.df["PX_LOW"]))**2 - (2 * np.log(2) - 1) * (np.log(self.df["PX_LAST"]) - np.log(self.df["PX_OPEN"]))**2
 
     def compute_momentum(self):
+        '''
+        Computes the momentum for the data
+        '''
         self.df['MOMENTUM'] = self.df['PX_LAST'] - self.df['PX_LAST'].shift(1)
 
     def compute_trend(self):
+        '''
+        Computes the trend for the price and volume data
+        '''
         self.df['PX_TREND'] = 2 * (self.df['PX_LAST'] - self.df['PX_OPEN']) / (self.df['PX_OPEN'] + self.df['PX_LAST'])
         self.df['PX_VTREND'] = self.df['PX_TREND'] * self.df['VOLUME']
 
-    def check_nan_values(self, trainX, testX, ahead): # check for nans in the data and print the number of nans
+    def check_nan_values(self, trainX, testX, ahead):
+        '''
+        Checks for NaN values in the data
+        '''
         if np.isinf(trainX).any() or np.isnan(trainX).any():
             print("Stock {} ahead: {} has nans.".format(self.ticker, ahead))
             print(trainX)
@@ -176,12 +185,22 @@ class Stock:
             print("Stock {} ahead: {} has nans.".format(self.ticker, ahead))
             print(testX)
 
-    def check_infinite_values(self, df): # check for infinite values in the data
+    def check_infinite_values(self, df):
+        '''
+        Checks for infinite values in the data
+        '''
         if np.isinf(df).values.sum() > 0:
             import pdb
             pdb.set_trace()
 
     def prepare_data_for_modeling(self, Xn, Yn, multi):
+        '''
+        Returns the data for modeling
+        
+        Parameters:
+        Xn - normalized features data
+        Yn - normalized target data
+        multi - boolean value for multivariate data'''
         pmod = int(Xn.shape[0] * self._tr_tst)
         if multi == False:  # Univariate
             trainX = Xn.iloc[:pmod,:]
@@ -196,14 +215,32 @@ class Stock:
         return trainX, trainY, testX, testY, pmod
     
     def process_stocks(self):
+        '''
+        Processes the stock data
+        '''
         self.compute_sentiment_scores()
         self.compute_volatility()
         self.compute_momentum()
         self.compute_trend()
         self.df.dropna(inplace=True)
 
+    def calculate_mean(self, data, axis=None):
+        # return np.mean(data, axis=axis)
+        return np.mean(data, axis=axis)
+
+    def calculate_min(self, data, axis=None):
+        # return np.min(data, axis=axis)
+        return np.min(data, axis=axis)
+
+    def calculate_max(self, data, axis=None):
+        # return np.max(data, axis=axis)
+        return np.max(data, axis=axis)
+
 
 def save_data(fich, out_path, lahead, lpar, tot_res):
+    '''
+    Saves the data to a pickle file
+    '''
     with open(fich, 'wb') as file:
             pickle.dump(out_path, file)
             pickle.dump(fich, file)
@@ -250,57 +287,63 @@ def main(args):
     # Load configuration parameters from the YAML file
     data_path = config['data']['data_path']
     out_path = config['data']['output_path']
-    win = config['serialization']['1D']['win']
-    tr_tst = config['serialization']['tr_tst']
-    lahead = config['serialization']['lahead']
-    n_ftrs = config['serialization']['n_features']
-    ticker = config['ticker']
+    tickers_list = config['tickers']
+    filename_structure = config['data']['filename_structure']
 
-    # stock_list = ["AAPL", "ADBE", "AMZN", "AVGO", "CMCSA", "COST", "CSCO", "GOOG", "GOOGL", "META", "MSFT", "NVDA", "PEP", "TMUS", "TSLA"]
+        # stock_list = ["AAPL", "ADBE", "AMZN", "AVGO", "CMCSA", "COST", "CSCO", "GOOG", "GOOGL", "META", "MSFT", "NVDA", "PEP", "TMUS", "TSLA"]
+    for ticker in tickers_list:
 
-    fich = os.path.join(data_path, f"{ticker} US Equity_060124.csv")
-    assert os.path.exists(fich), f"El archivo {fich} no existe."
-    data = pd.read_csv(fich, sep=",", index_col=0, parse_dates=True)
+        filename = filename_structure.format(ticker=ticker, date=args.date)
+        file = os.path.join(data_path, filename)
+        assert os.path.exists(file), f"El archivo {file} no existe."
 
-    stock = Stock(ticker, data, lahead, tr_tst)
-    stock.process_stocks()
+        scen = args.scenario
+        for scenario in config['serialization']['scenarios']:
+            if scenario['name'] == f'scenario_{scen}':
+                win = scenario['win']
+                tr_tst = scenario['tr_tst']
+                lahead = scenario['lahead']
+                n_ftrs = scenario['n_features']
+                break
 
-    # Univariate data processing
+        stock = Stock(ticker, file, lahead, tr_tst)
+        stock.process_stocks()
 
-    stock.process_univariate_data(win)
-    fdat1 = os.path.join(out_path, "{:02}/{:03}-input-output.pkl".format(win, ticker))
-    lpar  = [win, tr_tst]
+        # Univariate data processing
 
-    # Save univariate data
-    if os.path.exists(fdat1):
-        save_data(fdat1, out_path, lahead, lpar, stock.serial_dict)
-    else:
-        directory = os.path.dirname(fdat1)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            print(f"Directory {directory} created.")
+        stock.process_univariate_data(win)
+        fdat1 = os.path.join(out_path, "{:02}/{:03}-input-output.pkl".format(win, ticker))
+        lpar  = [win, tr_tst]
 
-        save_data(fdat1, out_path, lahead, lpar, stock.serial_dict)                
-        print(f"File {fdat1} created and data saved.")
+        # Save univariate data
+        if os.path.exists(fdat1):
+            save_data(fdat1, out_path, lahead, lpar, stock.serial_dict)
+        else:
+            directory = os.path.dirname(fdat1)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                print(f"Directory {directory} created.")
 
-    # Multivariate data processing
-    mwin = config['serialization']['mD']['win']
+            save_data(fdat1, out_path, lahead, lpar, stock.serial_dict)                
+            print(f"File {fdat1} created and data saved.")
 
-    stock.process_multivariate_data(mwin, n_ftrs)
-    fdat2 = os.path.join(out_path, "{:02}/{:03}-m-input-output.pkl".format(mwin, ticker))
-    lpar = [mwin, n_ftrs, tr_tst]
+        # Multivariate data processing
 
-    # Save multivariate data
-    if os.path.dirname(fdat2):
-        save_data(fdat2, out_path, lahead, lpar, stock.mserial_dict)
-    else:
-        directory = os.path.dirname(fdat2)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            print(f"Directory {directory} created.")
+        stock.process_multivariate_data(win, n_ftrs)
+        fdat2 = os.path.join(out_path, "{:02}/{:03}-m-input-output.pkl".format(win, ticker))
+        lpar = [win, n_ftrs, tr_tst]
 
-        save_data(fdat2, out_path, lahead, lpar, stock.mserial_dict)
-        print(f"File {fdat2} created and data saved.")
+        # Save multivariate data
+        if os.path.dirname(fdat2):
+            save_data(fdat2, out_path, lahead, lpar, stock.mserial_dict)
+        else:
+            directory = os.path.dirname(fdat2)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                print(f"Directory {directory} created.")
+
+            save_data(fdat2, out_path, lahead, lpar, stock.mserial_dict)
+            print(f"File {fdat2} created and data saved.")
 
 
 if __name__ == "__main__":
@@ -308,5 +351,7 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--verbose", nargs='?', action=VAction,\
             dest='verbose', help="Option for detailed information")
     parser.add_argument("params_file", help="Path to the configuration YAML file.")
+    parser.add_argument("--date", help="Date for filename structure")
+    parser.add_argument("--scenario", help="Number of scenario to process", type=int)
     args = parser.parse_args()
     main(args)
